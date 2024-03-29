7d34f8d89fb47dd23c817d6f3319351398906d8d
bdebiase <debiase2@gmail.com>
2024-03-29 18:18:47 -0400

    ags: make workspaces button monitor dependent

--- ags/widget/bar/buttons/Workspaces.ts
+++ ags/widget/bar/buttons/Workspaces.ts
@@ -9,34 +9,37 @@ const dispatch = (arg: string | number) => {
     sh(`hyprctl dispatch workspace ${arg}`)
 }
 
-const Workspaces = (ws: number) => Widget.Box({
-    children: range(ws || 20).map(i => Widget.Label({
-        attribute: i,
-        vpack: "center",
-        label: `${i}`,
-        setup: self => self.hook(hyprland, () => {
-            self.toggleClassName("active", hyprland.active.workspace.id === i)
-            self.toggleClassName("occupied", (hyprland.getWorkspace(i)?.windows || 0) > 0)
+const Workspaces = (ws: number, monitor: number) => {
+    const offset = monitor * 10
+    return Widget.Box({
+        children: range(ws || 10).map(i => Widget.Label({
+            attribute: i + offset,
+            vpack: "center",
+            label: `${i + offset}`,
+            setup: self => self.hook(hyprland, () => {
+                self.toggleClassName("active", hyprland.active.workspace.id === i + offset)
+                self.toggleClassName("occupied", (hyprland.getWorkspace(i + offset)?.windows || 0) > 0)
 
-            const distance = Math.abs(hyprland.active.workspace.id - i);
-            self.toggleClassName("small", distance > 1)
-            self.toggleClassName("smaller", distance > 2)
-        }),
-    })),
-    setup: box => {
-        if (ws === 0) {
-            box.hook(hyprland.active.workspace, () => box.children.map(btn => {
-                btn.visible = hyprland.workspaces.some(ws => ws.id === btn.attribute)
-            }))
-        }
-    },
-})
+                const distance = Math.abs(hyprland.active.workspace.id - i + offset)
+                self.toggleClassName("small", distance > 1)
+                self.toggleClassName("smaller", distance > 2)
+            }),
+        })),
+        setup: box => {
+            if (ws === 0) {
+                box.hook(hyprland.active.workspace, () => box.children.map(btn => {
+                    btn.visible = hyprland.workspaces.some(ws => ws.id === btn.attribute)
+                }))
+            }
+        },
+    })
+}
 
-export default () => PanelButton({
+export default (monitor: number) => PanelButton({
     window: "overview",
     class_name: "workspaces",
     on_scroll_up: () => dispatch("m+1"),
     on_scroll_down: () => dispatch("m-1"),
     on_clicked: () => App.toggleWindow("overview"),
-    child: workspaces.bind().as(Workspaces),
+    child: workspaces.bind().as(w => Workspaces(w, monitor)),
 })
